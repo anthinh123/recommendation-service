b055920d49675dc6819e72d466548f492fd2f627
thinh <anvanthinh@gmail.com>
2025-04-29 00:11:30 +0700

    init

---
+++ src/test/java/com/thinh/recommendation_service/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinh.recommendation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
